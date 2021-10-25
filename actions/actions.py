56a6e9c803ff3f749bc66a3c656d3f15ddc98651
Giustino98 <Giustino98@gmail.com>
2021-10-25 17:32:10 +0200

    Profonda modifica ai file actions, domain, nlu e stories. E' il primo step per consentire al chatbot di rilevare la stanza in cui si trova l'utente e suggerire l'acquisto di oggetti sulla base di tale informazione.

--- actions/actions.py
+++ actions/actions.py
@@ -5,7 +5,6 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
 from typing import Any, Text, Dict, List
 import time
 
@@ -15,6 +14,14 @@ from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 
 from pymongo import MongoClient
+
+import torch
+from torch.autograd import Variable as V
+import torchvision.models as models
+from torchvision import transforms as trn
+from torch.nn import functional as F
+import os
+from PIL import Image
 
 from rasa_sdk.types import DomainDict
 
@@ -34,10 +41,14 @@ class ValidateShowItem(FormValidationAction):
                       DomainDict,
                       ) -> Dict[Text, Any]:
         items = tracker.get_slot("items")
+        items_not_detected = tracker.get_slot("items_not_detected")
         if len(items) > 0:
             if slot_value in items:
                 return {"item": slot_value}
-        dispatcher.utter_message(text=f"L'oggetto {slot_value} non è tra quelli individuati dalla webcam.")
+        if len(items_not_detected) > 0:
+            if slot_value in items_not_detected:
+                return {"item": slot_value}
+        dispatcher.utter_message(text=f"L'oggetto {slot_value} non è tra quelli suggeriti.")
         return {"item": None}
 
 
@@ -64,11 +75,11 @@ class ActionShowItem(Action):
         dispatcher.utter_message(
             text="Perfetto, ti mostro n." + str(num_items) + " opzioni che hai a disposizione.")
         for j in range(0, num_items):
-          #  img = Image.open(item_db['image' + str(j + 1)])
-          #  img.show(title=item_chosen + str(j + 1))
+            #  img = Image.open(item_db['image' + str(j + 1)])
+            #  img.show(title=item_chosen + str(j + 1))
             link = str(item_db['link' + str(j + 1)])
             dispatcher.utter_message(text="Link n." + str(j + 1) + "= " + link)
-            dispatcher.utter_message(image=item_db['image' + str(j + 1)] )
+            dispatcher.utter_message(image=item_db['image' + str(j + 1)])
 
         return []
 
@@ -89,12 +100,36 @@ class ActionStartWebcam(Action):
     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
+    # th architecture to use
+    arch = 'resnet18'
+
+    # load the pre-trained weights
+    model_file = '%s_places365.pth.tar' % arch
+
+    model = models.__dict__[arch](num_classes=365)
+    checkpoint = torch.load(model_file, map_location=lambda storage, loc: storage)
+    state_dict = {str.replace(k, 'module.', ''): v for k, v in checkpoint['state_dict'].items()}
+    model.load_state_dict(state_dict)
+    model.eval()
+
+    # load the image transformer
+    centre_crop = trn.Compose([
+        trn.Resize((256, 256)),
+        trn.CenterCrop(224),
+        trn.ToTensor(),
+        trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    # load the class label
+    file_name = 'categories_places365.txt'
+
     def name(self) -> Text:
         return "action_start_webcam"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    async def run(
+            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        # custom behavior
 
         with open(self.classesFile, 'rt') as f:
             classNames = f.read().rstrip('\n').split('\n')
@@ -138,18 +173,50 @@ class ActionStartWebcam(Action):
 
             if k == ord('q'):
                 if len(objects) > 0:
-                    for i in range(0, len(objects)):
-                        dispatcher.utter_message(text="Oggetto n." + str(i + 1) + ": " + objects[i] + "\n")
-                #    dispatcher.utter_message(template="utter_user_will")
+                    # for i in range(0, len(objects)):
+                    #    dispatcher.utter_message(text="Oggetto n." + str(i + 1) + ": " + objects[i] + "\n")
+                    #    dispatcher.utter_message(template="utter_user_will")
                     object_detected = True
                 else:
-                  #  dispatcher.utter_message(text="Nessun oggetto rilevato. Vuoi ripetere la scansione?")
+                    #  dispatcher.utter_message(text="Nessun oggetto rilevato. Vuoi ripetere la scansione?")
                     object_detected = False
                 loop = False
                 cv2.destroyAllWindows()
                 cap.release()
 
-        return [SlotSet("items", objects), SlotSet("object_detected", object_detected)]
+        classes = list()
+        with open(self.file_name) as class_file:
+            for line in class_file:
+                classes.append(line.strip().split(' ')[0][3:])
+        classes = tuple(classes)
+
+        # load the test image
+        #   img_name = 'DemoPictures/diningroom_demo.jpg'
+
+        #  img = Image.open(img_name)
+        cv2_im = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        pil_im = Image.fromarray(cv2_im)
+        input_img = V(self.centre_crop(pil_im).unsqueeze(0))
+
+        # forward pass
+        logit = self.model.forward(input_img)
+        h_x = F.softmax(logit, 1).data.squeeze()
+        probs, idx = h_x.sort(0, True)
+
+        houseroom_indexes = [203, 215, 121, 45, 52]
+        probs_temp = []
+        idx_temp = []
+        for i in range(0, len(probs)):
+            if idx[i] in houseroom_indexes:
+                probs_temp.append(probs[i])
+                idx_temp.append(idx[i])
+
+        idx = idx_temp
+        room = classes[idx[0]]
+        #  dispatcher.utter_message(text="Vedo che sei nella stanza: " + classes[
+        #     idx[0]] + ". Sei interessato ad acquistare uno dei seguenti oggetti?")
+
+        return [SlotSet("items", objects), SlotSet("object_detected", object_detected), SlotSet("room", room)]
 
     def getNetOutput(self, blob):
         self.net.setInput(blob)
@@ -194,3 +261,57 @@ class ActionStartWebcam(Action):
                 objects.append(classnames[classIds[i]])
 
         return objects
+
+
+class UpdateObjects(Action):
+
+    def name(self) -> Text:
+        return "action_update_objects"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        items = tracker.get_slot("items")
+        object_detected = tracker.get_slot("object_detected")
+        if object_detected:
+            for i in range(0, len(items)):
+                dispatcher.utter_message(text="Oggetto n." + str(i + 1) + ": " + items[i] + "\n")
+                dispatcher.utter_message(template="utter_user_will")
+        else:
+            dispatcher.utter_message(text="Nessun oggetto rilevato. Vuoi ripetere la scansione?")
+
+        return []
+
+
+class NewObjects(Action):
+
+    def name(self) -> Text:
+        return "action_new_objects"
+
+    async def run(
+            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        # custom behavior
+        room = tracker.get_slot("room")
+        items = tracker.get_slot("items")
+
+        MONGODB_HOST = "localhost"
+        MONGODB_PORT = 27017
+
+        client = MongoClient(MONGODB_HOST, MONGODB_PORT)
+
+        db = client['RasaChatbot']
+        collection = db.RoomItems
+
+        item_db = collection.find_one({'room': room})
+
+        dispatcher.utter_message(text="Vedo che sei nella stanza: " + str(
+            room) + ". Potresti essere interessato all'acquisto di uno dei seguenti oggetti.")
+        items_not_detected = []
+        for i in range(0, len(item_db) - 2):
+            if item_db['item' + str(i + 1)] not in items:
+                dispatcher.utter_message(text="Oggetto nr." + str(i + 1) + ": " + item_db['item' + str(i + 1)])
+                items_not_detected.append(item_db['item' + str(i + 1)])
+
+        return [SlotSet("items_not_detected", items_not_detected)]
